d02d8c3761d073e7f9c28ca2fac6d262055a8f62
Sakshi Choudhary <103510144+sakshi-1140@users.noreply.github.com>
2024-06-26 14:21:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
 function createTable() {
     //Write your code here
-  
+    const row =prompt("Input number of rows");
+    const col =prompt("Input number of columns");
+    const table=document.querySelector('#myTable');
+//console.log(table);
+
+for(let i=0;i<row;i++){
+
+    const newRow=document.createElement('tr');
+    //console.log(newRow);
+        for(let j=0;j<col;j++){
+            const newCol=document.createElement('td');
+            newCol.textContent=`Row-${i} Column-${j}`
+            newRow.append(newCol);
+        }
+        table.append(newRow);
+    }
+
 }
+
